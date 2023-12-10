27b26a32eb9ef2367d9d61421d12943b3fda1690
Aabid Nabi <tantryinfo98@gmail.com>
2023-12-10 17:23:36 +0530

    first push

---
+++ Task_1_Portfolio_Website/README.md
@@ -0,0 +1,55 @@
+## Portfolio-Website
+
+
+# Personal Website Project
+
+## Overview
+
+Welcome to my personal website project! This simple website serves as an introduction to who I am, featuring my name, a brief bio, contact information, and links to my social media profiles. This project is an excellent way for me to establish an online presence and practice HTML and CSS skills.
+<br>
+
+![portfolio](https://github.com/Aabidnabi/Smyroid-Infotech-T1/assets/69672207/01da5474-a813-4698-8302-8a5262adf1b8)
+
+## Project Structure
+
+The project is structured as follows:
+
+- `index.html`: The main HTML file containing the content of the website.
+- `style.css`: The CSS file responsible for styling the HTML content.
+- `assets/`: A directory to store any images used on the website.
+
+## Usage
+
+To view the website locally or make further modifications, follow these steps:
+
+1. **Clone the Repository:**
+    ```bash
+    git clone https://github.com/your-username/personal-website.git
+    cd personal-website
+    ```
+
+2. **Open in a Browser:**
+    - Double-click on `index.html` or open it with your preferred web browser to view the website.
+
+3. **Modify Content:**
+    - Open `index.html` in a text editor of your choice (e.g., VSCode, Sublime Text).
+    - Update the content within the HTML tags to reflect your personal information.
+
+4. **Styling (Optional):**
+    - If you want to customize the appearance, you can modify styles in the `style.css` file.
+
+5. **Preview Changes:**
+    - Refresh the browser to see your changes.
+
+6. **Commit and Push:**
+    - If you're satisfied with your changes, commit and push them to your GitHub repository.
+
+## Contact Information
+
+If you have any questions, suggestions, or just want to connect, feel free to reach out to me:
+Happy coding!
+
+
+
+
+
---
+++ Task_3_Commercial_Website_Multiple_Pages/Phone-Palaza-master/includes/footer.php
@@ -0,0 +1,49 @@
+<?php
+        include 'includes/modal.php';
+        ?>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="columnf">
+                <h3>PhonePlaza</h3>
+                <p><a href="about_us.php"><span style="color:white">Home</span></a></p>
+                <p><a href="contact_us.php"><span style="color:white">About Us</span></a></p>
+                <p><a href="about_us.php"><span style="color:white">Featured</span></a></p>
+                <p><a href="contact_us.php"><span style="color:white">New</span></a></p>
+                <p><a href="products.php"><span style="color:white">Pricing</span></a></p>
+                <p><a href="contact_us.php"><span style="color:white">Subscribe</span></a></p>
+
+
+            </div>
+
+            <div class="columnf">
+                <h3>Our Services</h3>
+                <p><a href="#" data-toggle="modal" data-target="#loginmodal"><span style="color:white">Log In</span></a>
+                </p>
+                <p><a href="signup.php"><span style="color:white">Sign Up</span></a></p>
+                <p><a href="signup.php"><span style="color:white">Contact us</span></a></p>
+                <p><a href="index.php"><span style="color:white">Free Shipping</span></a></p>
+                <p><a href="index.php"><span style="color:white">Gift Cards</span></a></p>
+                <p><a href="index.php"><span style="color:white">Contact us</span></a></p>
+
+            </div>
+            <div class="columnf">
+                <h3>Contact Us</h3>
+                <p>Contact:+91-919166666</p>
+
+            </div>
+
+            </br></br></br>
+
+            <div class="columnf">
+                <b>
+                    <p>Copyright &copy;<script>
+                        document.write(new Date().getFullYear());
+                        </script> All Rights Reserved | <a>Aabid Nabi</a>
+                    </p>
+                </b>
+            </div>
+
+        </div>
+    </div>
+</footer>
